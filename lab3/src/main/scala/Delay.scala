5a3a108632224ff323fa4063a447946f35ab754c
kunkunshark <s226672@student.dtu.dk>
2023-02-16 16:33:22 +0100

    Fixed DelaySpec

--- lab3/src/main/scala/Delay.scala
+++ lab3/src/main/scala/Delay.scala
@@ -10,11 +10,24 @@ class Delay extends Module {
 
   // ***** your code starts here *****
 
-  // below is dummy code to make this example compile
-  res := io.din
+  val Reg = RegNext(RegNext(io.din))
+  res := Reg
 
-  // val delayReg = Reg(UInt(4.W))
-  // delayReg := delayIn
+  // the above example is the most simplified code
+  // to make the two clock cycles delay
+
+  /*
+  refer to page 76 Chisel Book for more
+  info regarding creation of registers
+
+  // do both example twice to have two clock cycles delay
+  val delayReg = Reg(UInt(4.W)) // this creates like a variable without initializing the value
+  delayReg := io.din
+
+  val valReg = RegInit(0.U(4.W)) // this creates variable and initializes the value to 0 every start
+  valReg := io.din
+  */
+
   // ***** your code ends here *****
 
   io.dout := res
