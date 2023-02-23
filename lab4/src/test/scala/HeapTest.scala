621948a2a08e9e4c958e5d877b37c15e1105d2cd
kunkunshark <s226672@student.dtu.dk>
2023-02-23 16:23:34 +0100

    Extra Test Code for HeapTest 1 and 2

--- lab4/src/test/scala/HeapTest.scala
+++ lab4/src/test/scala/HeapTest.scala
@@ -30,13 +30,37 @@ class HeapTest extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.root.expect(220.U)
 
       // write more test code here
-
+      
+      // expect that it will neither be empty nor full
+      dut.io.full.expect(0.B)
+      dut.io.empty.expect(0.B)
     }
   }
 
   it should "assert empty after all numbers have been removed" in {
     test(new TestHeap) { dut =>
       // write your test code here
+
+      // remove any root, initialize as empty root
+      dut.io.op.poke(Operation.RemoveRoot)
+      // set VALID as "true" to initiate handshake
+      dut.io.valid.poke(1.B)
+
+      // step up the clock to start
+      dut.clock.step()
+
+      // set VALID as "false" after one clock cycle
+      dut.io.valid.poke(0.B)
+
+      // wait until READY is "true" again to start the handshake
+      // READY is "true" by stepping up the clock when READY is "false"
+      while(!dut.io.ready.peekBoolean())
+          dut.clock.step()
+      // after this step, one clock cycle is already done
+
+      // expect it to be empty and NOT full
+      dut.io.empty.expect(1.B)
+      dut.io.full.expect(0.B)
     }
   }
 
